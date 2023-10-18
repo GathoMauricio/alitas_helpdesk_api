764b29a07bb28b7b612832c191afc30adf347f53
OMJS2769 <mauricio2769@outlook.es>
2023-10-18 10:38:03 -0600

    test fcm

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('login', function () {
     ]);
 })->name('login');
 Route::get('generar_password/{token}', [\App\Http\Controllers\UserController::class, 'generarPassword']);
+
+Route::get('fcm', function () {
+    return App\Http\Controllers\NotificacionController::notificacionNuevoSeguimiento([
+        'user_contact_id' => 52,
+        'num_case' => 27,
+        'body' => 'Test de credenciales',
+        'case_id' => 52,
+    ]);
+});
