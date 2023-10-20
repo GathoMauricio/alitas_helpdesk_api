487e5855958127531d1812e2b5b13c24d31d3ab9
OMJS2769 <mauricio2769@outlook.es>
2023-10-20 03:45:14 -0600

    crud para casos version web

---
+++ resources/views/casos/seguimientos.blade.php
@@ -0,0 +1,22 @@
+<!-- Modal -->
+<div class="modal fade" id="modal_seguimientos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Segimientos</h5>
+            </div>
+            <div class="modal-body" id="modal_body_seguimientos">
+                <div class="card">
+                    ...
+                </div>
+            </div>
+            <div class="modal-footer">
+                <form action="POST" id="form_nuevo_seguimiento" style="width: 100%;">
+                    <input type="text" id="txt_nuevo_seguimiento" class="form-control"
+                        placeholder="Ingrese el texto aquí...">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('login', function () {
-    return response()->json([
-        'error' => 401,
-        'type' => 'Unauthorized'
-    ]);
-})->name('login');
-Route::get('generar_password/{token}', [\App\Http\Controllers\UserController::class, 'generarPassword']);
-
-Route::get('fcm', function () {
-    return App\Http\Controllers\NotificacionController::notificacionNuevoSeguimiento([
-        'user_contact_id' => 52,
-        'num_case' => 27,
-        'body' => 'Test de credenciales',
-        'case_id' => 52,
-    ]);
+Auth::routes(['register' => false]);
+Route::get('/', function () {
+    if (Auth::check()) {
+        return App\Http\Controllers\HomeController::index();
+    }
+    return view('auth.login');
 });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('generar_password/{token}', [\App\Http\Controllers\UserController::class, 'generarPassword']);
+Route::get('show_caso/{}', [App\Http\Controllers\CasoController::class, 'show']);
+Route::get('create_caso', [App\Http\Controllers\CasoController::class, 'create']);
+Route::post('store_caso', [App\Http\Controllers\CasoController::class, 'store']);
+Route::get('edit_caso/{id}', [App\Http\Controllers\CasoController::class, 'edit']);
+Route::put('update_caso/{id}', [App\Http\Controllers\CasoController::class, 'update']);
+Route::delete('delete_caso', [App\Http\Controllers\CasoController::class, 'delete']);
+Route::get('cargar_seguimientos', [App\Http\Controllers\CasoController::class, 'cargarSeguimientos']);
+Route::post('store_seguimiento', [App\Http\Controllers\CasoController::class, 'storeSeguimiento']);
+Route::get('cargar_adjuntos', [App\Http\Controllers\CasoController::class, 'cargarAdjuntos']);
+Route::post('store_adjunto', [App\Http\Controllers\CasoController::class, 'storeAdjunto']);
+
+Route::get('api-obtener-tipos-servicio', [\App\Http\Controllers\TipoServicioController::class, 'apiObtenerTiposServicio']);
