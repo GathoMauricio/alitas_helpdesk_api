8ae831eb610e3a9303a11df3281d8cc43669b533
OMJS2769 <mauricio2769@outlook.es>
2023-10-20 03:49:06 -0600

    hotfix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Auth::routes(['register' => false]);
-Route::get('/', function () {
+Route::any('/', function () {
     if (Auth::check()) {
         return App\Http\Controllers\HomeController::index();
     }
